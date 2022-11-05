ab27f13034e228773de0d2bec43ee67479e6345d
carlosevi94 <carlosevillabarcelo@gmail.com>
2022-11-05 12:17:29 +0100

    Ejercicio

---
+++ myprogram.py
@@ -0,0 +1,31 @@
+
+
+class Clase():
+    alumnos = None
+    aula = None
+    profesor = None 
+    asignaturas = None 
+
+    def __init__(self):
+        #Irene
+        pass
+
+    def add_alumno(self):
+        # Juan
+        pass
+    
+    def change_professor(self):
+        # Adrian
+        pass
+
+    def remove_alumno(self):
+        # Carmen
+        pass
+
+    def add_asignatura(self):
+        # Carlos
+        pass
+
+    def change_aula(self):
+        # Ariadna
+        pass

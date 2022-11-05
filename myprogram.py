b10c2e49feced5fc7e0a3e4ccfb6afc2c891ff34
juandelacuadra <juandelacuadra@gmail.com>
2022-11-05 12:58:15 +0100

    Commit Juan

---
+++ myprogram.py
@@ -0,0 +1,35 @@
+
+
+class Clase():
+    alumnos = None
+    aula = None
+    profesor = None 
+    asignaturas = None 
+
+    def __init__(self):
+        #Irene
+        pass
+
+    def add_alumno(self, alumno):
+         # Juan
+        '''
+        Añade un alumno al listado existente.
+        '''
+        self.alumnos.append(alumno)
+
+    
+    def change_professor(self):
+        # Adrian
+        pass
+
+    def remove_alumno(self):
+        # Carmen
+        pass
+
+    def add_asignatura(self):
+        # Carlos
+        pass
+
+    def change_aula(self):
+        # Ariadna
+        pass

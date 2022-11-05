7a84ca6b23986927796715e5fa3c712ed19a288b
“irenitamora@hotmail.com” <114437789+irenemoraf@users.noreply.github.com>
2022-11-05 12:34:27 +0100

    init

--- myprogram.py
+++ myprogram.py
@@ -6,9 +6,11 @@ class Clase():
     profesor = None 
     asignaturas = None 
 
-    def __init__(self):
-        #Irene
-        pass
+    def __init__(self, notas, curso, colegio):
+        self.notas = notas
+        self.curso = curso 
+        self.colegio = colegio
+
 
     def add_alumno(self):
         # Juan
